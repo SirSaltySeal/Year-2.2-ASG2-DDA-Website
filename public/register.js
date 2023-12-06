2100c46564636e18b3dd88e93fa5736804460c6c
SirSaltySeal <S10197904@connect.np.edu.sg>
2023-12-06 15:46:17 +0800

    Fixed Firebase Register

--- public/register.js
+++ public/register.js
@@ -1,32 +1,3 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); // prevent the form from submitting
-
-    // get the form inputs
-    var email = document.getElementById('exampleInputEmail1');
-    var password = document.getElementById('exampleInputPassword1');
-    var confirmPassword = document.getElementById('exampleInputPassword2');
-
-    // validate the inputs
-    if (!email.value) {
-        alert('Please enter your email');
-        return;
-    }
-
-    if (!password.value) {
-        alert('Please enter your password');
-        return;
-    }
-
-    if (!confirmPassword.value) {
-        alert('Please confirm your password');
-        return;
-    }
-
-    if (password.value !== confirmPassword.value) {
-        alert('Passwords do not match');
-        return;
-    }
-
-    // if everything is valid, submit the form
-    this.submit();
-});
+window.onload = function() {
+    document.querySelector('.register-form').classList.add('fade-in');
+};
